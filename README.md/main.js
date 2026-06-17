050acd11f9436078394ccbcb29a896d7fa06cca7
rinahbaker-ui <rinah.baker.developer@gmail.com>
2026-06-17 08:52:14 -0700

    Add files via upload

---
+++ README.md/main.js
@@ -0,0 +1,38 @@
+
+// Create a variable and display its data type
+var age = 25;
+document.write("The data type of age is: " + typeof age);
+document.write("<br><br>");
+
+// Combine a string and a number
+document.write("My age is " + 25);
+document.write("<br><br>");
+
+// Use the == operator
+document.write("10 == '10': " + (10 == "10"));
+document.write("<br>");
+
+// Use the === operator
+document.write("10 === '10': " + (10 === "10"));
+document.write("<br>");
+
+// Use the > operator
+document.write("10 > 5: " + (10 > 5));
+document.write("<br>");
+
+// Use the < operator
+document.write("5 < 10: " + (5 < 10));
+document.write("<br>");
+
+// Use the && operator
+document.write("(10 > 5 && 5 < 10): " + (10 > 5 && 5 < 10));
+document.write("<br>");
+
+// Use the || operator
+document.write("(10 > 20 || 5 < 10): " + (10 > 20 || 5 < 10));
+document.write("<br>");
+
+// Use the ! operator
+document.write("!(10 > 5): " + !(10 > 5));
+document.write("<br>");
+
